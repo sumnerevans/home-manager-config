3cfdaf31c98dd406d2d44a4cbb0467763db7a571
Sumner Evans <me@sumnerevans.com>
2021-03-13 01:32:13 -0700

    Fix bug with picom config

--- services/xorg.nix
+++ services/xorg.nix
@@ -48,44 +48,6 @@ in
         "class_g = 'i3-frame'"
         "_GTK_FRAME_EXTENTS@:c"
       ];
-
-      extraOptions = let
-        # Basically a tinkered lib.generators.mkKeyValueDefault
-        # It either serializes a top-level definition "key: { values };"
-        # or an expression "key = { values };"
-        mkAttrsString = top:
-          mapAttrsToList (
-            k: v:
-              let
-                sep = if (top && isAttrs v) then ":" else "=";
-              in
-                "${escape [ sep ] k}${sep}${mkValueString v};"
-          );
-
-        # This serializes a Nix expression to the libconfig format.
-        mkValueString = v:
-          if types.bool.check v then boolToString v
-          else if types.int.check v then toString v
-          else if types.float.check v then toString v
-          else if types.str.check v then "\"${escape [ "\"" ] v}\""
-          else if builtins.isList v then "[ ${concatMapStringsSep " , " mkValueString v} ]"
-          else if types.attrs.check v then "{ ${concatStringsSep " " (mkAttrsString false v) } }"
-          else throw ''
-            invalid expression used in option services.picom.settings:
-            ${v}
-          '';
-
-        toConf = attrs: concatStringsSep "\n" (mkAttrsString true attrs);
-      in
-        toConf {
-          wintypes = {
-            tooltip = { shadow = true; opacity = 0.9; focus = true; full-shadow = false; };
-            dock = { shadow = false; };
-            dnd = { shadow = false; };
-            popup_menu = { opacity = 0.9; };
-            dropdown_menu = { opacity = 0.9; };
-          };
-        };
     };
   };
 }
