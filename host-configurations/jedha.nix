385db1230bcb8344960e8348ea350377e3ad10c9
Sumner Evans <me@sumnerevans.com>
2021-04-05 10:52:37 -0600

    Add second battery for jedha

--- host-configurations/jedha.nix
+++ host-configurations/jedha.nix
@@ -2,4 +2,13 @@
   xorg.enable = true;
   laptop.enable = true;
   networking.interfaces = [ "enp0s31f6" "wlp4s0" ];
+  programs.i3status-rust.extraBlocks = [
+    {
+      block = "battery";
+      interval = 30;
+      format = "{percentage}% {time}";
+      device = "BAT1";
+      priority = 111;
+    }
+  ];
 }
