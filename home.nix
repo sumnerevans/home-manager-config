bab10a014f4bd53f6706134591e2c9be2ab9fd81
Sumner Evans <me@sumnerevans.com>
2021-05-13 00:46:19 -0600

    overlays: removed python-mpv overlay
    https://github.com/NixOS/nixpkgs/pull/116462 was merged

--- home.nix
+++ home.nix
@@ -5,29 +5,6 @@
     ./host-config.nix
   ];
 
-  nixpkgs.overlays = [
-    (
-      self: super: rec {
-        python3 = super.python3.override {
-          packageOverrides = self2: super2: {
-            mpv = super2.buildPythonPackage rec {
-              pname = "mpv";
-              version = "0.5.2";
-              src = super.fetchFromGitHub {
-                owner = "jaseg";
-                repo = "python-mpv";
-                rev = "v${version}";
-                sha256 = "0ffskpynhl1252h6a05087lvpjgn1cn2z3caiv3i666dn1n79fjd";
-              };
-              propagatedBuildInputs = [ super.mpv ];
-              doCheck = false;
-            };
-          };
-        };
-      }
-    )
-  ];
-
   home = {
     stateVersion = "21.03";
     username = "sumner";
