8628d8da905eb77773d0fc4e2d12ffaccacd431e
Sumner Evans <me@sumnerevans.com>
2021-05-24 11:17:03 -0600

    element: 1.7.28 -> 1.7.29

--- modules/communication.nix
+++ modules/communication.nix
@@ -1,9 +1,35 @@
 { config, lib, pkgs, ... }: with lib; with pkgs; {
-  home.packages = optionals (config.wayland.enable || config.xorg.enable) [
-    discord
-    element-desktop
-    fractal
-    mumble
-    zoom-us
+  nixpkgs.overlays = [
+    (self: super: {
+      element-desktop = super.element-desktop.overrideAttrs (old: rec {
+        version = "1.7.29";
+        src = fetchFromGitHub {
+          owner = "vector-im";
+          repo = "element-desktop";
+          rev = "v${version}";
+          sha256 = "sha256-nCtgVVOdjZ/OK8gMInBbNeuJadchDYUO2UQxEmcOm4s=";
+        };
+
+        packageJSON = ./element-desktop-package.json;
+      });
+
+      element-web = super.element-web.overrideAttrs (old: rec {
+        version = "1.7.29";
+        src = fetchurl {
+          url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
+          sha256 = "sha256-wFC0B9v0V3JK9sLKH7GviVO/JEjePOJ06PwRq/MVqDE=";
+        };
+      });
+    })
   ];
+
+  home.packages = optionals
+    (config.wayland.enable || config.xorg.enable)
+    [
+      discord
+      element-desktop
+      fractal
+      mumble
+      zoom-us
+    ];
 }
