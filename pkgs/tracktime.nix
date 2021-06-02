7e29e96dea6a2b635b41011279e20f544135d8c9
Sumner Evans <me@sumnerevans.com>
2021-06-02 08:54:20 -0600

    tracktime: 0.9.18 -> 0.9.19

--- pkgs/tracktime.nix
+++ pkgs/tracktime.nix
@@ -1,7 +1,7 @@
 { lib, pkgs }: with pkgs;
 python38Packages.buildPythonApplication rec {
   pname = "tracktime";
-  version = "0.9.18";
+  version = "0.9.19";
 
   propagatedBuildInputs = with python38Packages; [
     argcomplete
@@ -20,7 +20,7 @@ python38Packages.buildPythonApplication rec {
 
   src = python38.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "a083b336945ce208d28173cf240b89b8e9adfceb145dfe82744c0ebef966cc99";
+    sha256 = "sha256-iHshWTPTIX4ztC2EQeWJ/fb/8I1f5q/eNA6JE4IwlKo=";
   };
 
   meta = with lib; {
