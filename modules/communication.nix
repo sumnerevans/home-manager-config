5d3c25b14e9a34c744a30893cdb7208ac4e68dcc
Sumner Evans <me@sumnerevans.com>
2021-06-07 11:50:49 -0600

    element: 1.7.29 -> 1.7.30

--- modules/communication.nix
+++ modules/communication.nix
@@ -1,26 +1,33 @@
 { config, lib, pkgs, ... }: with lib; with pkgs; {
   nixpkgs.overlays = [
-    (self: super: {
-      element-desktop = super.element-desktop.overrideAttrs (old: rec {
-        version = "1.7.29";
-        src = fetchFromGitHub {
-          owner = "vector-im";
-          repo = "element-desktop";
-          rev = "v${version}";
-          sha256 = "sha256-nCtgVVOdjZ/OK8gMInBbNeuJadchDYUO2UQxEmcOm4s=";
-        };
+    (
+      self: super: {
+        element-desktop = super.element-desktop.overrideAttrs (
+          old: rec {
+            version = "1.7.30";
+            src = fetchFromGitHub {
+              owner = "vector-im";
+              repo = "element-desktop";
+              rev = "v${version}";
+              sha256 = "09k1xxmzqvw8c1x9ndsdvwj4598rdx9zqraz3rmr3i58s51vycxp";
+            };
 
-        packageJSON = ./element-desktop-package.json;
-      });
+            packageJSON = ./element/element-desktop-package.json;
+            yarnNix = ./element/element-desktop-yarndeps.nix;
+          }
+        );
 
-      element-web = super.element-web.overrideAttrs (old: rec {
-        version = "1.7.29";
-        src = fetchurl {
-          url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
-          sha256 = "sha256-wFC0B9v0V3JK9sLKH7GviVO/JEjePOJ06PwRq/MVqDE=";
-        };
-      });
-    })
+        element-web = super.element-web.overrideAttrs (
+          old: rec {
+            version = "1.7.30";
+            src = fetchurl {
+              url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
+              sha256 = "1pnmgdyacxfk8hdf930rqqvqrcvckc3m4pb5mkznlirsmw06nfay";
+            };
+          }
+        );
+      }
+    )
   ];
 
   home.packages = optionals
