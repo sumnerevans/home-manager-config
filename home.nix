6a3a93e9edb5f7a3283a97be3441c85726c064c4
Sumner Evans <me@sumnerevans.com>
2021-06-22 21:25:20 -0600

    pantalaimon: remove config

--- home.nix
+++ home.nix
@@ -13,12 +13,4 @@
 
   # Always restart/start/stop systemd services on home manager switch.
   systemd.user.startServices = "sd-switch";
-
-  # services.pantalaimon = {
-  #   enable = true;
-  #   settings = {
-  #     Default = {LogLevel="Debug";SSL=true;};
-  #     local-matrix = {Homeserver="https://matrix.sumnerevans.com";ListenAddress="127.0.0.1";ListenPort=8008;};
-  #   };
-  # };
 }
