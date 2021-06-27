63cdd800bd4ecb8059655e936ea7548fa7396021
Sumner Evans <me@sumnerevans.com>
2021-06-27 09:55:46 -0600

    element-desktop: move to upstream

--- modules/communication.nix
+++ modules/communication.nix
@@ -1,34 +1,34 @@
 { config, lib, pkgs, ... }: with lib; with pkgs; {
-  nixpkgs.overlays = [
-    (
-      self: super: {
-        element-desktop = super.element-desktop.overrideAttrs (
-          old: rec {
-            version = "1.7.30";
-            src = fetchFromGitHub {
-              owner = "vector-im";
-              repo = "element-desktop";
-              rev = "v${version}";
-              sha256 = "09k1xxmzqvw8c1x9ndsdvwj4598rdx9zqraz3rmr3i58s51vycxp";
-            };
+  # nixpkgs.overlays = [
+  #   (
+  #     self: super: {
+  #       element-desktop = super.element-desktop.overrideAttrs (
+  #         old: rec {
+  #           version = "1.7.30";
+  #           src = fetchFromGitHub {
+  #             owner = "vector-im";
+  #             repo = "element-desktop";
+  #             rev = "v${version}";
+  #             sha256 = "0000000000000000000000000000000000000000000000000000";
+  #           };
 
-            packageJSON = ./element/element-desktop-package.json;
-            yarnNix = ./element/element-desktop-yarndeps.nix;
-          }
-        );
+  #           packageJSON = ./element/element-desktop-package.json;
+  #           yarnNix = ./element/element-desktop-yarndeps.nix;
+  #         }
+  #       );
 
-        element-web = super.element-web.overrideAttrs (
-          old: rec {
-            version = "1.7.30";
-            src = fetchurl {
-              url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
-              sha256 = "1pnmgdyacxfk8hdf930rqqvqrcvckc3m4pb5mkznlirsmw06nfay";
-            };
-          }
-        );
-      }
-    )
-  ];
+  #       element-web = super.element-web.overrideAttrs (
+  #         old: rec {
+  #           version = "1.7.30";
+  #           src = fetchurl {
+  #             url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
+  #             sha256 = "0000000000000000000000000000000000000000000000000000";
+  #           };
+  #         }
+  #       );
+  #     }
+  #   )
+  # ];
 
   home.packages = optionals (config.wayland.enable || config.xorg.enable)
     [
