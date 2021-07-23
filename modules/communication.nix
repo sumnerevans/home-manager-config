42b3c4fa06523f282cc6c35b67627997c639e996
Sumner Evans <me@sumnerevans.com>
2021-07-23 13:37:14 -0600

    element: use upstream with no overlays

--- modules/communication.nix
+++ modules/communication.nix
@@ -1,35 +1,4 @@
 { config, lib, pkgs, ... }: with lib; with pkgs; {
-  # nixpkgs.overlays = [
-  #   (
-  #     self: super: {
-  #       element-desktop = super.element-desktop.overrideAttrs (
-  #         old: rec {
-  #           version = "1.7.30";
-  #           src = fetchFromGitHub {
-  #             owner = "vector-im";
-  #             repo = "element-desktop";
-  #             rev = "v${version}";
-  #             sha256 = "0000000000000000000000000000000000000000000000000000";
-  #           };
-
-  #           packageJSON = ./element/element-desktop-package.json;
-  #           yarnNix = ./element/element-desktop-yarndeps.nix;
-  #         }
-  #       );
-
-  #       element-web = super.element-web.overrideAttrs (
-  #         old: rec {
-  #           version = "1.7.30";
-  #           src = fetchurl {
-  #             url = "https://github.com/vector-im/element-web/releases/download/v${version}/element-v${version}.tar.gz";
-  #             sha256 = "0000000000000000000000000000000000000000000000000000";
-  #           };
-  #         }
-  #       );
-  #     }
-  #   )
-  # ];
-
   home.packages = optionals (config.wayland.enable || config.xorg.enable)
     [
       discord
