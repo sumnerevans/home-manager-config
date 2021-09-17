e511d9087af4abd8abc2fd22e181729637b75462
Sumner Evans <me@sumnerevans.com>
2021-09-17 10:32:06 -0400

    yubikey: add yubikey-touch-detector pkg and service

--- modules/gpg.nix
+++ modules/gpg.nix
@@ -1,5 +1,9 @@
-{ pkgs, ... }: let
+{ config, lib, pkgs, ... }: let
   agentTTL = 60 * 60 * 4; # 4 hours
+  waylandCfg = config.wayland;
+  xorgCfg = config.xorg;
+
+  yubikey-touch-detector = pkgs.callPackage ../pkgs/yubikey-touch-detector.nix {};
 in
 {
   programs.gpg.enable = true;
@@ -11,5 +15,21 @@ in
     maxCacheTtl = agentTTL;
     pinentryFlavor = "gnome3";
     verbose = true;
+  };
+
+
+  systemd.user.services.yubikey-touch-detector = lib.mkIf (waylandCfg.enable || xorgCfg.enable) {
+    Unit = {
+      Description = "YubiKey touch detector";
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "${yubikey-touch-detector}/bin/yubikey-touch-detector --libnotify";
+      Restart = "always";
+      RestartSec = 5;
+    };
+
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
