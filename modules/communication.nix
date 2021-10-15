655c514afde04d525809dea74b67cb1272b39c77
Sumner Evans <me@sumnerevans.com>
2021-10-15 09:37:00 -0600

    element: fix seshat

--- modules/communication.nix
+++ modules/communication.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }: with lib; with pkgs; let
-  beeper-desktop = pkgs.callPackage ../pkgs/beeper-desktop.nix {};
-in{
+  beeper-desktop = pkgs.callPackage ../pkgs/beeper-desktop.nix { };
+  element-desktop = pkgs.element-desktop.overrideAttrs (old: {
+    fixupPhase = ''
+      sed -i "s#exec#LD_PRELOAD=${pkgs.sqlcipher}/lib/libsqlcipher.so exec#g" "$out/bin/element-desktop"
+    '';
+  });
+in
+{
   home.packages = optionals (config.wayland.enable || config.xorg.enable)
     [
       beeper-desktop
