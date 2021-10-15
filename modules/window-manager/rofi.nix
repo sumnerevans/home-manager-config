04622df7243bf70bd491c5c00e2cd7c64270832b
Sumner Evans <me@sumnerevans.com>
2021-10-15 09:36:47 -0600

    rofi: update config for new format

--- modules/window-manager/rofi.nix
+++ modules/window-manager/rofi.nix
@@ -1,16 +1,15 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   programs.rofi = {
     enable = true;
     font = "pango:Iosevka 12";
-    lines = 15;
     location = "top";
     terminal = "${pkgs.alacritty}/bin/alacritty";
-    theme = "solarized_alternate";
-    width = 25;
     yoffset = 80;
     extraConfig = {
       matching = "fuzzy";
     };
+
+    theme = "Arc-Dark";
+    pass.enable = true;
   };
-  programs.rofi.pass.enable = true;
 }
