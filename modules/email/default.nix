f17f79dbad78a8e11ed8bd67e4f73390c21fd178
Sumner Evans <me@sumnerevans.com>
2022-01-13 13:12:14 -0700

    email: add patch to notmuch to make it compile

--- modules/email/default.nix
+++ modules/email/default.nix
@@ -15,6 +15,19 @@ in
   services.imapnotify.enable = true;
   programs.msmtp.enable = true;
 
+  nixpkgs.overlays = [
+    (self: super: {
+      notmuch = super.notmuch.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          (super.fetchpatch {
+            url = "https://raw.githubusercontent.com/stigtsp/nixpkgs/b0770408976ec309f70e19dd78599bf8f2ad7145/pkgs/applications/networking/mailreaders/notmuch/test-fix-support-for-gpgsm-in-gnupg-2.3.patch";
+            sha256 = "sha256-Zaushz7+/b+P3Fcjb4huENTN74QUbJ6c3fVPuZd75kk=";
+          })
+        ];
+      });
+    })
+  ];
+
   home.file = {
     "${quotesPath}" = {
       source = ./quotes;
