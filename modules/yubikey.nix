493946daf07c4c1a9b034f5b4a864203afa52f7f
Sumner Evans <me@sumnerevans.com>
2022-01-13 13:13:06 -0700

    yubikey: disable management tools because of build failures

--- modules/yubikey.nix
+++ modules/yubikey.nix
@@ -1,8 +1,8 @@
 { config, lib, pkgs, ... }: {
-  home.packages = with pkgs; [
-    yubikey-manager
-  ] ++ lib.optionals (config.wayland.enable || config.xorg.enable) [
-    yubikey-manager-qt
-    yubioath-desktop
-  ];
+  # home.packages = with pkgs; [
+  #   yubikey-manager
+  # ] ++ lib.optionals (config.wayland.enable || config.xorg.enable) [
+  #   yubikey-manager-qt
+  #   yubioath-desktop
+  # ];
 }
