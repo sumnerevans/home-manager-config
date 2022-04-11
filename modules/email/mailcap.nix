e441c86762ba174b9a90d0bbd6e6c3837a463fa0
Sumner Evans <me@sumnerevans.com>
2022-04-11 07:30:50 -0600

    email/mailcap: reformat

--- modules/email/mailcap.nix
+++ modules/email/mailcap.nix
@@ -1,37 +1,36 @@
-{ pkgs, ... }: let
+{ pkgs, ... }:
+let
+  feh = "${pkgs.feh}/bin/feh";
+  libreoffice = "${pkgs.libreoffice}/bin/libreoffice";
+  icalviewScript = pkgs.writeScript "icalview" (builtins.readFile ./icalview.py);
 in
 {
-  xdg.configFile."neomutt/mailcap".text = let
-    feh = "${pkgs.feh}/bin/feh";
-    libreoffice = "${pkgs.libreoffice}/bin/libreoffice";
-    icalviewScript = pkgs.writeScript "icalview" (builtins.readFile ./icalview.py);
-  in
-    ''
-      # HTML
-      text/html; ${pkgs.elinks}/bin/elinks -dump %s; copiousoutput;
+  xdg.configFile."neomutt/mailcap".text = ''
+    # HTML
+    text/html; ${pkgs.elinks}/bin/elinks -dump %s; copiousoutput;
 
-      # PDF documents
-      application/pdf; ${pkgs.zathura}/bin/zathura %s
+    # PDF documents
+    application/pdf; ${pkgs.zathura}/bin/zathura %s
 
-      # Images
-      image/jpg; ${feh} %s
-      image/jpeg; ${feh} %s
-      image/pjpeg; ${feh} %s
-      image/png; ${feh} %s
-      image/gif; ${feh} %s
+    # Images
+    image/jpg; ${feh} %s
+    image/jpeg; ${feh} %s
+    image/pjpeg; ${feh} %s
+    image/png; ${feh} %s
+    image/gif; ${feh} %s
 
-      # iCal
-      text/calendar; ${icalviewScript}; copiousoutput
-      application/calendar; ${icalviewScript}; copiousoutput
-      application/ics; ${icalviewScript}; copiousoutput
+    # iCal
+    text/calendar; ${icalviewScript}; copiousoutput
+    application/calendar; ${icalviewScript}; copiousoutput
+    application/ics; ${icalviewScript}; copiousoutput
 
-      # Office Suites
-      application/msword; ${libreoffice} %s;
-      application/vnd.ms-word.document.12; ${libreoffice} %s;
-      application/vnd.openxmlformats-officedocument.wordprocessingml.document; ${libreoffice} %s;
-      application/vnd.oasis.opendocument.text; ${libreoffice} %s;
+    # Office Suites
+    application/msword; ${libreoffice} %s;
+    application/vnd.ms-word.document.12; ${libreoffice} %s;
+    application/vnd.openxmlformats-officedocument.wordprocessingml.document; ${libreoffice} %s;
+    application/vnd.oasis.opendocument.text; ${libreoffice} %s;
 
-      # Microsoft LookOut
-      application/ms-tnef; ${pkgs.tnef}/bin/tnef -w -C /home/sumner/tmp %s;
-    '';
+    # Microsoft LookOut
+    application/ms-tnef; ${pkgs.tnef}/bin/tnef -w -C /home/sumner/tmp %s;
+  '';
 }
