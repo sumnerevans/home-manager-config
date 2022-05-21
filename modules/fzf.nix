7ad5b97f0f3baae8b6b7029290304dc6a1c93963
Sumner Evans <me@sumnerevans.com>
2022-05-21 10:52:15 -0600

    fzf: ignore .git/ harder

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -1,9 +1,8 @@
-{ config, lib, pkgs, ... }: with lib;
-{
+{ config, lib, pkgs, ... }: with lib; {
   programs.fzf = {
     enable = true;
     changeDirWidgetOptions = [ "--preview 'tree -C {} | head -200'" ];
-    defaultCommand = "fd --type f --hidden --follow";
+    defaultCommand = "fd --type f --hidden --follow --exclude '.git'";
     defaultOptions = [ "--border" "--height 40%" ];
     fileWidgetOptions = [ "--preview 'bat --style=numbers --color=always --line-range :500 {}'" ];
   };
