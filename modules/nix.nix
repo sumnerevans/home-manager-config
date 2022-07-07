e9914c7972948b7ee1fa4d354bd9cd8278d12bb5
Sumner Evans <me@sumnerevans.com>
2022-07-07 17:19:56 -0600

    cachix: add nix-community
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,14 +1,23 @@
-{
+{ pkgs, ... }: {
   imports = [
     (
       let
-        declCachix = builtins.fetchTarball "https://github.com/jonascarpay/declarative-cachix/archive/1986455ab3e55804458bf6e7d2a5f5b8a68defce.tar.gz";
+        declCachix = builtins.fetchTarball "https://github.com/jonascarpay/declarative-cachix/archive/b63e9cb0d0962656d08fb4668a5b6b1ba1ca5648.tar.gz";
       in
       import "${declCachix}/home-manager.nix"
     )
   ];
 
+  home.packages = [ pkgs.cachix ];
+
   caches.cachix = [
-    { name = "sumnerevans"; sha256 = "111dzynldwpisdrj138kql1v9a62k5p7bk00fcjm2gvlbma2d9bv"; }
+    {
+      name = "sumnerevans";
+      sha256 = "sha256:042yrl18pfiqnjmy7rmiq0rsi8kldvi88w2g1lv90y40pmcq9zdy";
+    }
+    {
+      name = "nix-community";
+      sha256 = "sha256:1955r436fs102ny80wfzy99d4253bh2i1vv1x4d4sh0zx2ssmhrk";
+    }
   ];
 }
