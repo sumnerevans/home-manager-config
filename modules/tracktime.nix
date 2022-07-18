13e920f5307a560422f2193d59c9642454e048b0
Sumner Evans <me@sumnerevans.com>
2022-07-18 12:00:57 -0600

    tracktime/hspc: gitlab -> github
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/tracktime.nix
+++ modules/tracktime.nix
@@ -74,7 +74,7 @@ in
     tt-beeper = "tt start -c Beeper";
     tt-bri = "tt start -t linear -c Beeper -p BRI";
     tt-element = "tt start -c Beeper 'Element catchup'";
-    tt-hspc = "tt start -t gl -p ColoradoSchoolOfMines/hspc-problems";
+    tt-hspc = "tt start -t gh -p ColoradoSchoolOfMines/hspc-problems";
     tt-tea = "tt start -p teaching/ppl";
     tt-issues = "tt start -t gl -p 'beeper/issues' -c Beeper";
     tt-standup = "tt start -c Beeper 'Standup'";
