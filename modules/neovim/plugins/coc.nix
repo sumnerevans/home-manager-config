fb304a79ada3199b9fbce8d8662c45e6a27e7c2d
Sumner Evans <me@sumnerevans.com>
2022-08-22 23:43:16 +0200

    neovim/coc: fix tab
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/neovim/plugins/coc.nix
+++ modules/neovim/plugins/coc.nix
@@ -84,10 +84,20 @@
 
         " Use tab for trigger completion with characters ahead and navigate.
         inoremap <silent><expr> <TAB>
-              \ pumvisible() ? "\<C-n>" :
-              \ <SID>check_back_space() ? "\<TAB>" :
-              \ coc#refresh()
-        inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+          \ coc#pum#visible() ? coc#pum#next(1):
+          \ CheckBackspace() ? "\<Tab>" :
+          \ coc#refresh()
+        inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+        " Make <CR> to accept selected completion item or notify coc.nvim to format
+        " <C-g>u breaks current undo, please make your own choice.
+        inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                                      \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+        
+        function! CheckBackspace() abort
+          let col = col('.') - 1
+          return !col || getline('.')[col - 1]  =~# '\s'
+        endfunction
 
         function! s:check_back_space() abort
           let col = col('.') - 1
