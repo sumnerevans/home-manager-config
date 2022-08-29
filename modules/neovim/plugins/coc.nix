ced379dd07f7df510e4f8f63e20ce18835189ef7
Sumner Evans <me@sumnerevans.com>
2022-08-29 17:24:55 -0600

    neovim/coc: fix for new version
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/neovim/plugins/coc.nix
+++ modules/neovim/plugins/coc.nix
@@ -32,6 +32,8 @@
         "explorer.icon.enableNerdfont" = true;
         "explorer.position" = "right";
 
+        "suggest.noselect" = true;
+
         "languageserver" = {
           "nix" = {
             "command" = "rnix-lsp";
@@ -84,22 +86,12 @@
 
         " Use tab for trigger completion with characters ahead and navigate.
         inoremap <silent><expr> <TAB>
-          \ coc#pum#visible() ? coc#pum#next(1):
+          \ coc#pum#visible() ? coc#pum#next(1) :
           \ CheckBackspace() ? "\<Tab>" :
           \ coc#refresh()
         inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
-
-        " Make <CR> to accept selected completion item or notify coc.nvim to format
-        " <C-g>u breaks current undo, please make your own choice.
-        inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
-                                      \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
         
         function! CheckBackspace() abort
-          let col = col('.') - 1
-          return !col || getline('.')[col - 1]  =~# '\s'
-        endfunction
-
-        function! s:check_back_space() abort
           let col = col('.') - 1
           return !col || getline('.')[col - 1]  =~# '\s'
         endfunction
