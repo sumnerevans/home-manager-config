db119e582c003749274d340e8d0413a751d2aee0
Sumner Evans <me@sumnerevans.com>
2022-10-25 09:06:59 -0600

    copilot: update to 1.6.0
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/neovim/plugins/copilot.nix
+++ modules/neovim/plugins/copilot.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }: with pkgs; let
   copilot = pkgs.vimUtils.buildVimPluginFrom2Nix rec {
     pname = "copilot";
-    version = "0.1.0";
+    version = "1.6.0";
     src = pkgs.fetchFromGitHub {
       owner = "github";
       repo = "copilot.vim";
-      rev = "c2e75a3a7519c126c6fdb35984976df9ae13f564";
-      sha256 = "sha256-V13La54aIb3hQNDE7BmOIIZWy7In5cG6kE0fti/wxVQ=";
+      rev = "8ba151a20bc1d7a5c72e592e51bfc925d5bbb837";
+      sha256 = "sha256-GmwR+S5sQnVUbVShP53jNpSKMZaoeh9Rf37v89wAJ3M=";
     };
     meta.homepage = "https://github.com/features/copilot";
   };
