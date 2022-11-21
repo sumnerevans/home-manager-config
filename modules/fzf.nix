b6a7dd16925289659a501df58a63ddda9f7ff8ca
Sumner Evans <me@sumnerevans.com>
2022-11-21 08:36:00 -0700

    fzf: add theme for fzf prompt
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -5,5 +5,20 @@
     defaultCommand = "fd --type f --hidden --follow --exclude '.git'";
     defaultOptions = [ "--border" "--height 40%" ];
     fileWidgetOptions = [ "--preview 'bat --style=numbers --color=always --line-range :500 {}'" ];
+    colors = {
+      fg = "-1";
+      bg = "-1";
+      hl = "#c678dd";
+      "fg+" = "#ffffff";
+      "bg+" = "#4b5263";
+      "hl+" = "#d858fe";
+      info = "#98c379";
+      prompt = "#61afef";
+      pointer = "#be5046";
+      marker = "#e5c07b";
+      spinner = "#61afef";
+      header = "#61afef";
+    };
+    tmux.enableShellIntegration = true;
   };
 }
