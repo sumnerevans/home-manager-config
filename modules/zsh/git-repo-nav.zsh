be6d5fff5fb69607dd5806a60a706870060dad6c
Sumner Evans <me@sumnerevans.com>
2023-05-16 12:44:46 -0600

    zsh/git-repo-nav: add spaces to prevent it from showing in history
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/zsh/git-repo-nav.zsh
+++ modules/zsh/git-repo-nav.zsh
@@ -10,7 +10,7 @@ fzf-git-repo-nav() {
     return 0
   fi
   zle push-line # Clear buffer. Auto-restored on next prompt.
-  BUFFER="builtin cd -- $root/${(q)dir}"
+  BUFFER="  builtin cd -- $root/${(q)dir}"
   zle accept-line
   local ret=$?
   unset dir # ensure this doesn't end up appearing in prompt expansion
