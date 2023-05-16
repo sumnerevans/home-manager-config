e56603ebc91d84212fbd386f88be9201b330b2f6
Sumner Evans <me@sumnerevans.com>
2023-05-16 16:34:12 -0600

    shell: convert to flake
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -8,10 +8,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
     declarative-cachix.url = "github:jonascarpay/declarative-cachix";
   };
 
-  outputs = { nixpkgs, home-manager, declarative-cachix, ... }:
+  outputs = { nixpkgs, home-manager, declarative-cachix, flake-utils, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -29,5 +30,21 @@
     in
     {
       homeConfigurations."tatooine" = mkConfig ./host-configurations/tatooine.nix;
-    };
+    } // (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { system = system; };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              black
+              cargo
+              git-crypt
+              pre-commit
+              rnix-lsp
+            ];
+          };
+        }
+      ));
 }
