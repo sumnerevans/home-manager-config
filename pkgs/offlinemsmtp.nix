eefaaeb49394e47a945d65ab6b90dd6772494025
Sumner Evans <me@sumnerevans.com>
2023-06-23 21:43:01 -0600

    offlinemsmtp: upgrade
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- pkgs/offlinemsmtp.nix
+++ pkgs/offlinemsmtp.nix
@@ -1,18 +1,17 @@
 { lib, pkgs }: with pkgs;
 python3Packages.buildPythonApplication rec {
   pname = "offlinemsmtp";
-  version = "0.4.0";
-  format = "pyproject";
+  version = "unstable-2023-06-23";
+  format = "flit";
 
   src = pkgs.fetchFromGitHub {
     owner = "sumnerevans";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-k9X7TCiBDq3fDlCZa0eyH3Z6IDCvVjpGvQCoO4bsw5Q=";
+    rev = "9dd0a8eec84aa62dfd4a4a7eae62e0733917e9b2";
+    sha256 = "sha256-V4CDnpG8PZHs/0vA+6DvS5v9o08WRCQ0CKwmK1rGuY0=";
   };
 
   nativeBuildInputs = [
-    python3Packages.poetry-core
     gobject-introspection
     python3Packages.setuptools
     wrapGAppsHook
