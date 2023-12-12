ad57581075dace61f24b6ab0b6e45e35221e045c
Sumner Evans <me@sumnerevans.com>
2023-12-12 09:24:45 -0700

    beeper-desktop: don't waylandify
    It was showing up as a blank screen, probably because of the Electron
    version having a regression.
    
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/communication.nix
+++ modules/communication.nix
@@ -4,12 +4,7 @@ let beeper-desktop = pkgs.callPackage ../pkgs/beeper-desktop.nix { };
 in {
   home.packages = with pkgs;
     [ gomuks ] ++ optionals (config.wayland.enable || config.xorg.enable) [
-      (if config.wayland.enable then
-        pkgs.writeShellScriptBin "beeper-desktop" ''
-          ${beeper-desktop}/bin/beeper-beta --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations
-        ''
-      else
-        beeper-desktop)
+      beeper-desktop
       discord
       element-desktop
       mumble
