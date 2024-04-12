603cfac57b7e557efec09d83ec8671253d8738f5
Sumner Evans <me@sumnerevans.com>
2024-04-12 10:46:26 -0600

    communication: only enable slack in work mode
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/communication.nix
+++ modules/communication.nix
@@ -3,14 +3,7 @@ with lib;
 let beeper-desktop = pkgs.callPackage ../pkgs/beeper-desktop.nix { };
 in {
   home.packages = with pkgs;
-    [ gomuks ] ++ optionals (config.wayland.enable || config.xorg.enable) [
-      beeper-desktop
-      discord
-      element-desktop
-      mumble
-      neochat
-      nheko
-      slack
-      zoom-us
-    ];
+    [ gomuks ] ++ (optionals (config.wayland.enable || config.xorg.enable)
+      ([ beeper-desktop discord element-desktop mumble zoom-us ]
+        ++ optionals config.work.enable [ slack ]));
 }
