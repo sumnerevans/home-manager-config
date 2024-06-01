4dc9b093f628a32b938519e3e8086436d0518066
Sumner Evans <sumner.evans@automattic.com>
2024-05-31 22:59:46 -0600

    kitty: limit scrollback to a million lines
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- modules/window-manager/kitty.nix
+++ modules/window-manager/kitty.nix
@@ -2,7 +2,7 @@
   programs.kitty = {
     enable = true;
     settings = {
-      scrollback_lines = -1;
+      scrollback_lines = 1000000;
       enable_audio_bell = false;
       update_check_interval = 0;
       allow_hyperlinks = true;
