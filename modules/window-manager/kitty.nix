0a6b5809a02275ed962918c27e2c098d8103849e
Sumner Evans <sumner.evans@automattic.com>
2024-10-29 13:09:28 -0600

    window-manager/kitty: less scrollback lines
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- modules/window-manager/kitty.nix
+++ modules/window-manager/kitty.nix
@@ -2,7 +2,7 @@
   programs.kitty = {
     enable = true;
     settings = {
-      scrollback_lines = 1000000;
+      scrollback_lines = 100000;
       enable_audio_bell = false;
       update_check_interval = 0;
       allow_hyperlinks = true;
