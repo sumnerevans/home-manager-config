52a9b0dd377546ab02d086f561bb4ddaa98d67d0
Sumner Evans <me@sumnerevans.com>
2025-03-04 15:30:08 -0700

    browsers: install brave
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/browsers.nix
+++ modules/browsers.nix
@@ -1,18 +1,17 @@
 { config, lib, pkgs, ... }:
-with lib;
-let
-  chromeCommandLineArgs = "-high-dpi-support=0 -force-device-scale-factor=1";
-  hasGui = config.wayland.enable || config.xorg.enable;
+let hasGui = config.wayland.enable || config.xorg.enable;
 in {
-  home.packages = with pkgs;
-    [ elinks w3m ] ++ (optionals hasGui [
-      (google-chrome.override { commandLineArgs = chromeCommandLineArgs; })
-    ]);
+  home.packages = with pkgs; [ elinks w3m ];
 
+  programs.google-chrome.enable = hasGui;
+  programs.brave = {
+    enable = hasGui;
+    commandLineArgs = [ "--enable-features=TouchpadOverscrollHistoryNavigation" ];
+  };
   programs.chromium.enable = hasGui;
   programs.firefox.enable = hasGui;
 
-  home.sessionVariables = mkIf hasGui {
+  home.sessionVariables = lib.mkIf hasGui {
     # Enable touchscreen in Firefox
     MOZ_USE_XINPUT2 = "1";
     MOZ_DBUS_REMOTE = "1";
