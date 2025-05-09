a10b6c37c01a40ec40f7aa010bab05560dc87b88
Sumner Evans <me@sumnerevans.com>
2025-05-08 19:42:19 -0600

    neovim: add additional barbar mappings
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/neovim/plugins/barbar.nix
+++ modules/neovim/plugins/barbar.nix
@@ -8,6 +8,9 @@
         map <A-1> <Cmd>BufferPrevious<CR>
         map <A-2> <Cmd>BufferNext<CR>
         map <C-W> <Cmd>BufferClose<CR>
+
+        map <A-[> <Cmd>BufferPrevious<CR>
+        map <A-]> <Cmd>BufferNext<CR>
       '';
     }
   ];
