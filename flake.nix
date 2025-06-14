6f50bfa3672ab3f41b24928ec5582c773cbc23ae
Sumner Evans <me@sumnerevans.com>
2025-06-14 10:07:47 -0600

    flake.lock: update nixpkgs,home-manager
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -33,25 +33,6 @@
             overlays = [
               (final: prev: { inherit (menucalc.packages.${system}) menucalc; })
               (final: prev: { inherit (mdf.packages.${system}) mdf; })
-              (final: prev: {
-                harper = prev.rustPlatform.buildRustPackage rec {
-                  pname = "harper";
-                  version = "0.34.1";
-
-                  src = prev.fetchFromGitHub {
-                    owner = "Automattic";
-                    repo = "harper";
-                    rev = "v${version}";
-                    hash =
-                      "sha256-fBAPJhB+x8cIFs6rp1nDvrtVkAKx2wuFCO7FwHOwLRM=";
-                  };
-
-                  buildAndTestSubdir = "harper-ls";
-                  useFetchCargoVendor = true;
-                  cargoHash =
-                    "sha256-XVC2xgUwazYXVp5sx6kA+aopd9m38XlbgsLnb0D92kg=";
-                };
-              })
             ];
           };
 
